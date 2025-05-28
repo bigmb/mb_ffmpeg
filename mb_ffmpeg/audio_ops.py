748bcd3599cb0cef810eab2fef80318c2a25f44c
bigmb <malav.b93@gmail.com>
2025-05-28 23:05:54 +0100

    updated version.py to 1.0.12

--- mb_ffmpeg/audio_ops.py
+++ mb_ffmpeg/audio_ops.py
@@ -33,7 +33,7 @@ class AudioOperations(FFmpegBase):
         command = self.build_command(
             input_file,
             output_file,
-            ["-vn", "-acodec" ,"libmp3lame" if output_format == "mp3" else "copy"]
+            ["-vn", "-acodec" ,"-y","libmp3lame" if output_format == "mp3" else "copy"]
         )
         
         self._run_command(command)
